43aa118bdf07549b7b44c677e106ebc8cb92d9d0
totorowg <wangchingying@gmail.com>
2018-01-09 13:15:06 +0800

    用程式寫menu

--- app/src/main/java/com/cyw/a2018010901/MainActivity.java
+++ app/src/main/java/com/cyw/a2018010901/MainActivity.java
@@ -17,15 +17,24 @@ public class MainActivity extends AppCompatActivity {
     //直接打option就會出現method讓你選, 這個option是手機右上角三個點
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        menu.add("ADD");
+        menu.add(0,1,0,"ADD");
+        menu.add(0,2,0,"EDIT");
+        //老師沒講Group是幹嘛用的也沒講order
+//        menu.add(1,1,0,"ADD1");
+//        menu.add(1,2,0,"EDIT1");
+
         return super.onCreateOptionsMenu(menu);
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        if(item.getTitle().equals("ADD"))
-        {
-            Toast.makeText(MainActivity.this,"你按了ADD",Toast.LENGTH_SHORT).show();
+        switch (item.getItemId()) {
+            case 1:
+                Toast.makeText(MainActivity.this, "你按了ADD", Toast.LENGTH_SHORT).show();
+                break;
+            case 2:
+                Toast.makeText(MainActivity.this, "你按了EDIT", Toast.LENGTH_SHORT).show();
+                break;
         }
         return super.onOptionsItemSelected(item);
     }
